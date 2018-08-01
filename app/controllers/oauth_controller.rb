9179fe994bc44d9bf8d802efa187937fcf255dac
Kang Peng <kangtingpeng@nypl.org>
2018-08-01 16:08:02 -0400

    Set up basic OAuth authentication work flow.

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -1,45 +1,47 @@
 class OauthController < ApplicationController
   def login
     require 'oauth2'
+    require 'securerandom'
 
     # TODO: Do we have ISSO log in cookies? Should we have one?
-
     # TODO: Check the URL link pattern to see if it matches the pattern /auth/provider or /callback
-
     # If no, check the log in status. If not log in
-    # redirect to the linke as below for putting in credientials
-    # https://isso.nypl.org/oauth/authorize?response_type=code&redirect_uri=http%3A%2F%2Flocal.nypl.org%3A3001%2Fcallback&scope=openid%20login%3Astaff&state=PBIXrTuYhHP1kwbK39YAgQCE&client_id=platform_admin
-    # The user submits the credentials on the ISSO form. ISSO from redirect the user back to the application
-    # When back to the application, sending the request to get access token
-    # With the access token, go to the first page
-
-    # TODO: we need an authorization to check if the user is on the white list of the scsbuster
 
     client_id     = CLIENT_ID
     client_secret = CLIENT_SECRET
     authorize_url = OAUTH_AUTH_URL
     token_url     = OAUTH_TOKEN_URL
-    redirect_uri  = OAUTH_CALLBACK_URL
+    redirect_uri  = 'http://local.nypl.org:3001/callback'
     login_url     = OAUTH_LOGIN_URL
 
-    client = OAuth2::Client.new(client_id, client_secret, :authorize_url => authorize_url, :token_url => token_url)
-    puts "REDIRECT: " + redirect_uri
-    puts "CLIENT_SECRET: " + client_secret
-    puts "CLIENT_ID: " + client_id
-    puts "AUTHORIZE_URL: " + authorize_url
-    puts "TOKEN_URL: " + token_url
-    puts "LOGIN URL: " + login_url
+    state = SecureRandom.alphanumeric(24)
 
-    client.auth_code.authorize_url(:redirect_uri => redirect_uri)
+    client = OAuth2::Client.new(client_id, client_secret, site: 'https://isso.nypl.org')
+    isso_url = client.auth_code.authorize_url(:redirect_uri => redirect_uri) + '&state=' + state + '&scope=login:staff'
 
-
-    # token = client.auth_code.get_token('authorization_code_value', :redirect_uri => 'http://localhost:3000/oauth2/callback', :headers => {'Authorization' => 'Basic some_password'})
-    # response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
-    # response.class.name
-    # # => OAuth2::Response
+    redirect_to isso_url
   end
 
   def callback
     puts 'callback!!'
+
+    redirect_uri  = 'http://local.nypl.org:3001/callback'
+    client_id     = CLIENT_ID
+    client_secret = CLIENT_SECRET
+
+    client = OAuth2::Client.new(client_id, client_secret, site: 'https://isso.nypl.org')
+    token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+
+    puts token.token
+    puts token.expires_in
+    puts token.refresh_token
+    puts token.options
+    puts token.params
+
+    # Now we can put "Authorization: bearer token.token" in the header when making an HTTP request
+    # TODO: we need an authorization to check if the user is on the white list of the scsbuster
+  end
+
+  def refresh_oauth_token
   end
 end
