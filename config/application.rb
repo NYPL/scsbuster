3969b1a78e5f0d88bc8c11fec0d3982576959a2b
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-08-02 13:45:14 -0400

    Allow logging to STDOUT
    The app will log to STDOUT if RAILS_LOG_TO_STDOUT is set.
    Logging to STDOUT in production will automatically send logs to cloudwatch

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ module Scsbuster
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Use default logging formatter so that PID and timestamp are not suppressed.
+    config.log_formatter = ::Logger::Formatter.new
+
+    # Use a different logger for distributed setups.
+    # require 'syslog/logger'
+    # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
+
+    if ENV["RAILS_LOG_TO_STDOUT"].present?
+      logger           = ActiveSupport::Logger.new(STDOUT)
+      logger.formatter = config.log_formatter
+      config.logger    = ActiveSupport::TaggedLogging.new(logger)
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
