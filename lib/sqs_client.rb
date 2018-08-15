72b2c3c4cfda51d648f3e74806b391ce44221b00
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-08-15 16:30:15 -0400

    Remove commented out code

--- lib/sqs_client.rb
+++ lib/sqs_client.rb
@@ -7,7 +7,7 @@ class SqsClient
       secret_access_key: AWS_SECRET
     )
   end
-  
+
   def send_message(entries)
     resp = @sqs.send_message_batch({
       queue_url: SQS_QUEUE_URL,
@@ -15,21 +15,4 @@ class SqsClient
     })
     resp
   end
-  # entries = [
-  #     {
-  #       id: self.barcodes.first,
-  #       message_body: JSON.generate({
-  #         barcodes: self.barcodes,
-  #         protectCGD: self.protect_cgd,
-  #         action: self.action,
-  #         email: self.user_email
-  #       })
-  #     }
-  #   ]
-  #
-  # resp = sqs.send_message_batch({
-  #   queue_url: queue_url,
-  #   entries: entries,
-  # })
-  # resp
 end
