9523c7229b4d03d152f9fb3c958aa8b3a5092003
Kang Peng <kangtingpeng@nypl.org>
2018-08-30 18:16:41 -0400

    Updated error_controller_spec.rb

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -3,17 +3,17 @@ class ErrorController < ApplicationController
   ERROR_DEFAULT = {
     message: 'The service is not available now.',
     instruction_text: 'Back to homepage',
-    instruction_link: '/',
+    instruction_link: '/'
   }
   ERROR_AUTHENTICATION_FAILED = {
     message: 'The authentication failed.',
     instruction_text: 'Back to homepage',
-    instruction_link: '/',
+    instruction_link: '/'
   }
   ERROR_NOT_AUTHORIZED = {
     message: 'The user is not authorized.',
     instruction_text: 'Log in with another account',
-    instruction_link: '/authenticate',
+    instruction_link: '/authenticate'
   }
 
   # Render the error page based on the error type
--- spec/controllers/error_controller_spec.rb
+++ spec/controllers/error_controller_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe ErrorController, :type => :controller do
         get :error, params: { type: "" }
       end
       it "should render the default error message and instruction" do
-        expect(assigns(:error_message)).to      eq('The service is not available now.')
-        expect(assigns(:instruction)).to        eq({ message: 'Back to homepage', link: root_path })
+        expect(assigns(:error)[:message]).to                 eq("The service is not available now.")
+        expect(assigns(:error)[:instruction_text]).to        eq("Back to homepage")
+        expect(assigns(:error)[:instruction_link]).to        eq(root_path)
       end
     end
 
@@ -17,8 +18,9 @@ RSpec.describe ErrorController, :type => :controller do
         get :error, params: { type: "not_listed_error" }
       end
       it "should render the default error message and instruction" do
-        expect(assigns(:error_message)).to      eq('The service is not available now.')
-        expect(assigns(:instruction)).to        eq({ message: 'Back to homepage', link: root_path })
+        expect(assigns(:error)[:message]).to                 eq("The service is not available now.")
+        expect(assigns(:error)[:instruction_text]).to        eq("Back to homepage")
+        expect(assigns(:error)[:instruction_link]).to        eq(root_path)
       end
     end
 
@@ -27,8 +29,20 @@ RSpec.describe ErrorController, :type => :controller do
         get :error, params: { type: "user_not_authorized" }
       end
       it "should render the error message and instruction about an unauthorized user" do
-        expect(assigns(:error_message)).to      eq('The user is not authorized.')
-        expect(assigns(:instruction)).to        eq({ message: 'Log in with another account', link: authenticate_path })
+        expect(assigns(:error)[:message]).to                 eq("The user is not authorized.")
+        expect(assigns(:error)[:instruction_text]).to        eq("Log in with another account")
+        expect(assigns(:error)[:instruction_link]).to        eq(authenticate_path)
+      end
+    end
+
+    context "when the query of type is authentication_failed" do
+      before do
+        get :error, params: { type: "authentication_failed" }
+      end
+      it "should render the error message and instruction about failed authentication" do
+        expect(assigns(:error)[:message]).to                 eq("The authentication failed.")
+        expect(assigns(:error)[:instruction_text]).to        eq("Back to homepage")
+        expect(assigns(:error)[:instruction_link]).to        eq(root_path)
       end
     end
   end
