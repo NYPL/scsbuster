b89d8723841fbef5e11ee6af11728342c50ec0c7
jbdalton <josephdalton@nypl.org>
2018-09-25 14:54:13 -0400

    cleanup and added empty and too-short barcode tests

--- spec/features/transfer_barcode_errors_spec.rb
+++ spec/features/transfer_barcode_errors_spec.rb
@@ -5,80 +5,20 @@ describe "SCSBuster transfer barcode errors" do
 	describe "When on the Transfer Barcode & Update Metadata page" do
 	
 		before(:each) do
-      skip_authentication_to(transfer_metadata_path)
+			skip_authentication_to(transfer_metadata_path)
+			cut_off_sqs!
 		end
 
-		describe "Given an incorrect barcode is entered" do 
-		
-			#describe "It should fail for these conditions:" do
-			
-				# 				before(:all) do
-				# 
-				# 					if (defined?(ENV['ERROR_BARCODE']) && ENV['ERROR_BARCODE'] != nil) then
-				# 						@this_error_barcode = ENV['ERROR_BARCODE']
-				# 					else
-				# 						#insert "good" slug here.
-				# 						@this_error_barcode = "33433047299254"
-				# 					end
-				# 
-				# 					if (defined?(ENV['ERROR_BNUMBER']) && ENV['ERROR_BNUMBER'] != nil) then
-				# 						@this_error_bnumber = ENV['ERROR_BNUMBER']
-				# 					else
-				# 						#insert "good" slug here.
-				# 						@this_error_bnumber = "b11556466x"
-				# 					end
-				# 
-				# 				
-				# 				end
+		describe "Given a correct bnumber and an invalid barcode is entered" do 
 
-				# this first block is for inline-testing of barcode errors, normally will be commented out
-
-				# 				if (defined?(ENV['ERROR_BARCODE']) && ENV['ERROR_BARCODE'] != nil) 
-				# 
-				# 					it " - generic error for #{ENV['ERROR_BARCODE']} displays:  (WIP)" do
-				# 
-				# 						aggregate_failures "check-generic" do
-				# 
-				# 							#puts "this_error_barcode: #{@this_error_barcode}/#{ENV['ERROR_BARCODE']}"
-				# 							#puts "this_error_bnumber: #{@this_error_bnumber}/#{ENV['ERROR_BNUMBER']}"
-				# 	
-				# 							save_snap("pre_generic","png")
-				# 							#save_snap("pre_generic","html")
-				# 				
-				# 							find(:xpath, "//input[@id='barcode']").set("#{@this_error_barcode}")
-				# 
-				# 							find(:xpath, "//input[@id='bib_record_number']").set("#{@this_error_bnumber}")
-				# 
-				# 							click_submit_value("submit")
-				# 
-				# 							sleep 2
-				# 							save_snap("post_generic","png")
-				# 							#save_snap("post_generic","html")
-				# 
-				# 							within('#flash_error') do
-				# 					
-				# 								expect((find(:xpath, "//div[@id='flash_error']/p").text).downcase).to include("must be 14 numerical digits in length")
-				# 
-				# 				
-				# 								#expect(fail-message).to eq("fail-message")
-				# 					
-				# 							end
-				# 					
-				# 						end
-				# 				
-				# 					end
-				# 					
-				# 				#/if-filter
-				# 				end
-
-				describe "When a non-numerical barcode is entered" do
+				describe "non-numerical barcodes" do
 					
 					before(:each) do
 						@this_error_barcode = 'x' * 14
 						@this_error_bnumber = "b11556466x"
 					end
 					
-					it "it should fail with error msg: must be 14 numerical digits in length" do
+					it "should fail non-numerical barcode with error msg" do
 
 						aggregate_failures "check-num" do
 
@@ -100,14 +40,14 @@ describe "SCSBuster transfer barcode errors" do
 				
 				end
 
-				describe	"When a too long barcode is entered" do
+				describe	"too long barcodes" do
 
 					before(:each) do
 						@this_error_barcode = '1' * 15
 						@this_error_bnumber = "b11556466x"
 					end
 
-					it "it should fail with error msg: must be 14 numerical digits in length" do
+					it "should fail too-long barcode with error msg" do
 
 						aggregate_failures "check-long" do
 
@@ -129,14 +69,14 @@ describe "SCSBuster transfer barcode errors" do
 				
 				end
 
-				describe	"When a too short barcode is entered" do
+				describe	"short barcodes" do
 
 					before(:each) do
 						@this_error_barcode = '1' * 13
 						@this_error_bnumber = "b11556466x"
 					end
 
-					it "it should fail with error msg: must be 14 numerical digits in length" do
+					it "should fail short barcode with error msg" do
 
 						aggregate_failures "check-short" do
 
@@ -158,83 +98,39 @@ describe "SCSBuster transfer barcode errors" do
 				
 				end
 
+				describe	"empty barcodes" do
 
+					before(:each) do
+						@this_error_barcode = ''
+						@this_error_bnumber = "b11556466x"
+					end
 
-# temp block
-# 				it " - too long barcode 12345678123456781234567 (error msg: must be 14 numerical digits in length)" do
-# 
-# 
-# 					aggregate_failures "check-long" do
-# 
-# 						save_snap("pre_long","png")
-# 						#save_snap("pre_long","html")
-# 				
-# 						find(:xpath, "//input[@id='barcode']").set("123456781234567812345678")
-# 						find(:xpath, "//input[@id='bib_record_number']").set("b11556466x")
-# 
-# 						click_submit_value("submit")
-# 
-# 						sleep 2
-# 						save_snap("post_long","png")
-# 						#save_snap("post_long","html")
-# 
-# 						within('#flash_error') do
-# 					
-# 							expect((find(:xpath, "//div[@id='flash_error']/p").text).downcase).to include("must be 14 numerical digits in length")
-# 
-# 				
-# 							#expect(fail-message).to eq("fail-message")
-# 					
-# 						end
-# 					
-# 					end
-# 				
-# 				#/it-block
-# 				end
-# 
-# 				it " - empty barcode (error msg: must be 14 numerical digits in length)" do
-# 
-# 					aggregate_failures "check-empty" do
-# 
-# 						save_snap("pre_empty","png")
-# 						#save_snap("pre_empty","html")
-# 				
-# 						find(:xpath, "//input[@id='barcode']").set("")
-# 						find(:xpath, "//input[@id='bib_record_number']").set("b11556466x")
-# 
-# 						click_submit_value("submit")
-# 
-# 						sleep 2
-# 						save_snap("post_empty","png")
-# 						#save_snap("post_empty","html")
-# 
-# 						within('#flash_error') do
-# 					
-# 							expect((find(:xpath, "//div[@id='flash_error']/p").text).downcase).to include("must be 14 numerical digits in length")
-# 
-# 				
-# 							#expect(fail-message).to eq("fail-message")
-# 					
-# 						end
-# 					
-# 					end
+					it "should fail empty barcode with error msg" do
+
+						aggregate_failures "check-short" do
+
+							find(:xpath, "//input[@id='barcode']").set("#{@this_error_barcode}")
+
+							find(:xpath, "//input[@id='bib_record_number']").set("#{@this_error_bnumber}")
+
+							click_submit_value("submit")
+
+							within('#flash_error') do
+								expect((find(:xpath, "//div[@id='flash_error']/p").text).downcase).to include("must be 14 numerical digits in length")
+							end
+					
+						#/aggregate_failures
+						end
 				
-
-				# 			<div class="nypl-text-field-with-label">
-				#         <label class="required">Barcode</label><span class="nypl-required-field"> Required</span>
-				#         <input type="text" name="barcode" id="barcode" value="" class="required">
-				#         <span class="nypl-field-status" id="barcodes-status" aria-live="assertive" aria-atomic="true">Enter a single barcode.</span>
-				#       </div>
-			
-				#       				#expect(fail-message).to eq("fail-message")
+					#/it-block
+					end
 				
-			
-			#/inner-inner-inner-describe-it-should-fail-for-these-conditions
-			#end
-		
-		#/inner-inner-describe-
+				end
+
+		#/inner-inner-describe-given-an-incorrect-barcode
 		end
 
+	#/inner-describe
 	end
 	
 end
