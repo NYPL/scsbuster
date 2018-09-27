0e5e8d620c36f2e98aaae45c02052a85b58e4ea2
nodanaonlyzuul <beholdthepanda@gmail.com>
2018-09-27 10:23:07 -0400

    Don't pollute test output in the console

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -18,6 +18,9 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{1.hour.to_i}"
   }
 
+  # Don't pollute test output in the console
+  config.logger = NyplLogFormatter.new(File.join(Rails.root, 'log', 'test.log'))
+
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
